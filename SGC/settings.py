673a04dbb2252ab73e058568ae070f6b77311eb1
FrancimarAlexandre <francimaralexandredt@gmail.com>
2025-11-01 07:24:18 -0300

    feat: criando a tela inicial

--- SGC/settings.py
+++ SGC/settings.py
@@ -27,6 +27,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my apps
+    'core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +107,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+# URL base para arquivos estáticos
+STATIC_URL = '/static/'
+
+# Pasta onde o Django vai reunir os arquivos estáticos (usada em produção)
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Pastas adicionais onde procurar arquivos estáticos
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # cria essa pasta no projeto
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
